afd0197afb764da5748362f7c37d1ee137bc052e
saicheong <yow.saicheong@gmail.com>
2019-04-25 22:48:15 +0800

    add clj-gatling

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject deps-retrieve "0.1.0-SNAPSHOT"
   :local-repo "local-m2/repository"
   :dependencies [[org.clojure/tools.logging "0.4.1"]
-                 [clj-http "3.9.1"]])
+                 [clj-http "3.9.1"]
+                 [clj-gatling "0.13.0"]])
